4b8958374de964509d06d7682c4d81bf8f99fbcf
kostiantynfedorenko <kostia.fedorenko@gmail.com>
2015-10-01 20:07:25 +0300

    Second commit

--- app/src/main/java/com/gmail/fedorenko/kostia/app1lesson4/Constants.java
+++ app/src/main/java/com/gmail/fedorenko/kostia/app1lesson4/Constants.java
@@ -1,8 +1,5 @@
 package com.gmail.fedorenko.kostia.app1lesson4;
 
-/**
- * Created by kfedoren on 01.10.2015.
- */
 public class Constants {
 
     public static final String REGION_SPINNER = "region spinner";
